ce30d45d51cecd43b50156472958dd6821b4203d
liquid11 <orangevolution@yahoo.com>
2015-08-12 21:02:03 +0500

    gem store

---
+++ app/app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by tezrosolutions on 12/08/2015.
+ */
+
+(function() {
+
+    var storeApp = angular.module('storeApp', []);
+
+    var gem = {
+        name:'Azurite' ,
+        price:'2.49' ,
+        description:'2,49' ,
+        canPurchase:false
+    }
+    storeApp.controller('StoreController',function() {
+    });
+})();
